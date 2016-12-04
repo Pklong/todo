82ea606321616606fbcae6a5058cf5545223e873
PKLong <patrickklong@gmail.com>
2016-12-04 13:14:28 -0500

    Add reset and spinner style

--- frontend/components/shared/spinner.js
+++ frontend/components/shared/spinner.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Spinner = () => (
-  <div>LOADING!!!!</div>
+  <div className='spinner'>
+    <div className='double-bounce1'></div>
+    <div className='double-bounce2'></div>
+  </div>
 )
 
 export default Spinner;
